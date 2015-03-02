cef9f06c7b817ac5020c8857bbe9b65ce4f7e16e
Danny Baumann <dannybaumann@web.de>
2015-03-02 10:33:31 +0100

    Set oppo-app parameter.
    Change-Id: Ie117200f3cedd0710c15115d3bcbdf8d2030665a

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -253,6 +253,8 @@ static char *camera_fixup_setparams(int id, const char *settings)
         }
     }
 
+    params.set("oppo-app", "1");
+
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);
     params.dump();
