2944e2ae82bb266aa906682d24e2d48347fe181c
Jason Riordan <jriordan001@gmail.com>
2015-11-13 16:55:36 -0500

    n3: build the common oppo kernel
    Change-Id: Iaf4c369ef45d8140d0bc2d1190ae1e1e9a0de252

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,9 +22,9 @@
 TARGET_SPECIFIC_HEADER_PATH += device/oppo/n3/include
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
-TARGET_KERNEL_CONFIG := cyanogenmod_n3_defconfig
-TARGET_KERNEL_SOURCE := kernel/oppo/n3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
+TARGET_KERNEL_CONFIG := msm8974_n3_defconfig
+TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := n3,N5206,N5207
