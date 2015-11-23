e9690190385ed66ce91b9d06eba7320907ea25b5
Michael Bestas <mikeioannina@gmail.com>
2015-11-23 22:58:42 +0200

    Adapt model name to camera blob expectations.
    * Don't mess with ro.build.product in custom init,
      only ro.product.model needs to be set
    * Also add missing copyright
    
    Change-Id: Id696384a2f4542c4d3da2dc39c83925254284319

--- cm.mk
+++ cm.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2014-2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 $(call inherit-product, device/oppo/n3/n3.mk)
 
 # Enhanced NFC
@@ -17,4 +31,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=OPPO/N5206/N3:4.4.4/KTU84P/1415366188:user/release-keys \
-    PRIVATE_BUILD_DESC="msm8974-user 4.4.4 KTU84P eng.root.20141129.010050 dev-keys"
+    PRIVATE_BUILD_DESC="msm8974-user 4.4.4 KTU84P eng.root.20141129.010050 dev-keys" \
+    TARGET_DEVICE="N3"
