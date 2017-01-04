6e4aced8346524f95392783aa5a082eb09efd79e
Michael Bestas <mikeioannina@gmail.com>
2017-01-04 22:39:08 +0200

    n3: Rebrand to LineageOS
    Change-Id: I53100d2186f6224998ddb90a46d4522ca2691a01

--- lineage.mk
+++ lineage.mk
@@ -18,10 +18,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from n3 device
 $(call inherit-product, device/oppo/n3/n3.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_n3
+PRODUCT_NAME := lineage_n3
 PRODUCT_DEVICE := n3
 PRODUCT_BRAND := OPPO
 PRODUCT_MANUFACTURER := OPPO
