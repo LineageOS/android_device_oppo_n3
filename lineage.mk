4c59c8d3ea0c8d97c60e1822bfc0671e970f8c48
Simon Shields <simon@lineageos.org>
2017-08-23 00:32:04 +1000

    n3: s@vendor/cm@vendor/lineage@g
    Change-Id: I472b1c9e31e81671bcb9e09f43d5c497395d662a

--- lineage.mk
+++ lineage.mk
@@ -18,8 +18,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from n3 device
 $(call inherit-product, device/oppo/n3/n3.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_n3
 PRODUCT_DEVICE := n3
