1db8a264175c413bdd62e4a6b19798bb7d99cafb
jrior001 <jriordan001@gmail.com>
2017-10-16 22:37:20 -0400

    DO NOT MERGE: n3: Permissive SELinux
    Change-Id: I721fdfcb51df8f5afed54687bbca14183f60eb4c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,7 @@
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_CONFIG := lineageos_n3_defconfig
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 
