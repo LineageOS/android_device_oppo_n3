9741172dfff196c44b8c6bdb81b74207bdcf6b0f
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 19:18:53 +0200

    n3: Add missing NFC HCE permission
    Change-Id: I51009f9b70ed1e8eef698505158674ba703a698f

--- n3.mk
+++ n3.mk
@@ -60,7 +60,8 @@ PRODUCT_COPY_FILES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/vendor/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/vendor/etc/permissions/android.hardware.nfc.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/vendor/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/vendor/etc/permissions/android.hardware.nfc.hce.xml
 
 # Model is set via init library
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
